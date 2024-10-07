d6408e2a8299f98494719a1e2dc29c2d1a60d092
Pasty-Owl-Off <sichyov.a@yandex.ru>
2024-10-07 16:47:36 +0300

    connect database

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -5,6 +5,9 @@ import jm.task.core.jdbc.model.User;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
+    private static final String CON_URL = "jdbc:mysql://localhost:3306/mysql";
+    private static final String USER_NAME = "root";
+    private static final String PASSWORD = "Root1234";
     public UserDaoJDBCImpl() {}
 
     public void createUsersTable() {
